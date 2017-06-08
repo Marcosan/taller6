b632df44b0e639966cb56fb268a0d03c1ecd5076
marcosan <marcoxavibsc@gmail.com>
2017-06-08 17:38:24 -0500

    [NEW] Funcion 3

--- funcion2.c
+++ funcion2.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 void desglosar(char *fecha);
+void intercambiar(int *a, int *b);
 
 int main()
 {
 	char *fecha = "2017-06-06";
+
+	int a = 2;
+	int b = 3;	
+
+	printf("Literal 1:\n");
 	desglosar(fecha);
+
+	printf("Literal 2:\n");
+
+	intercambiar(&a,&b);
+	printf("Literal 3:\n");
+	printf("Ahora a vale: %d\n",a);
+	printf("Y b vale: %d\n",b);
 }
 
 void desglosar(char *fecha){
@@ -14,7 +27,7 @@ void desglosar(char *fecha){
 	for (i = 0; i < strlen(fecha); i++){
 		if(*(fecha + i) != '-'){
 			if(i == 0){
-				printf("\nAño: ");
+				printf("Año: ");
 			}
 			printf("%c", *(fecha + i));
 		} else{
@@ -27,5 +40,11 @@ void desglosar(char *fecha){
 		}
 		
 	}
+	printf("\n\n");
 }
 
+void intercambiar(int* a, int* b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
